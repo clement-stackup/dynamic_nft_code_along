d24b9fe8774f386121580a9f199811d42a263f5d
clement-stackup <120361535+clement-stackup@users.noreply.github.com>
2023-07-24 16:09:14 +0800

    Update starter.sol

--- starter.sol
+++ starter.sol
@@ -6,13 +6,26 @@ import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.s
 import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
 
-contract dynNFT {
+contract dynNFT is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
 
     // Metadata information for each stage of the NFT on IPFS.
     string[] IpfsUri = [
         "https://bafkreicxerjedfae7jnvcnwzstiklowhijjyf4hu2gw2qtyznxjtzi4uve.ipfs.nftstorage.link",
         "https://bafkreigyzkba5myht24jwxw3wko2vejvjfzlgzaoyionztzek7wyvhrj2e.ipfs.nftstorage.link"
     ];
+
+    constructor() ERC721("dNFTs", dNFT") {
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, IpfsUri[0]);
+    }
 
 /*
 ********************
